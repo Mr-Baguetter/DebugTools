358aff5262c40c578850c42ebdd506abbe930a40
Mr. Baguetter <Mr.baguetter1@gmail.com>
2025-08-19 23:51:05 -0500

    Fixers

--- DebugTools/Commands/DebugCommands/MethodTiming.cs
+++ DebugTools/Commands/DebugCommands/MethodTiming.cs
@@ -1,38 +1,145 @@
-using CommandSystem;
+﻿using CommandSystem;
 using DebugTools.API.Interfaces;
 using DebugTools.Commands.Patches;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DebugTools.Commands.Debug
 {
     internal class MethodTiming : ISubcommand
     {
         public string Name { get; } = "debugtiming";
-
         public string Description { get; } = "Shows the time that methods used to execute";
-
-        public string VisibleArgs { get; } = "<Plugin Name>";
-
+        public string VisibleArgs { get; } = "<action> [plugin name]";
         public int RequiredArgsCount { get; } = 1;
-
         public string RequiredPermission { get; } = "debug.timing";
-
         public string[] Aliases { get; } = ["timing"];
 
         public bool Execute(List<string> args, ICommandSender sender, out string response)
-        {            
-            if (MethodTimer.isPatched)
+        {
+            string action = args[0].ToLower();
+
+            switch (action)
             {
-                MethodTimer.UnpatchAll();
-                response = $"Unpatched";
+                case "patch":
+                case "enable":
+                case "on":
+                    return HandlePatch(args.Skip(1).ToList(), out response);
+
+                case "unpatch":
+                case "disable":
+                case "off":
+                    return HandleUnpatch(args.Skip(1).ToList(), out response);
+
+                case "list":
+                case "status":
+                    return HandleList(out response);
+
+                case "unpackall":
+                case "clear":
+                case "stop":
+                    return HandleUnpatchAll(out response);
+
+                case "help":
+                case "?":
+                    return HandleHelp(out response);
+
+                default:
+                    response = $"Error: Unknown action '{action}'. Use 'debugtiming help' for a list of commands.";
+                    return false;
+            }
+        }
+
+        private bool HandlePatch(List<string> pluginArgs, out string response)
+        {
+            if (pluginArgs.Count == 0)
+            {
+                response = "Error: No plugin name specified. Usage: debugtiming patch <plugin name>";
+                return false;
+            }
+
+            string pluginName = string.Join(" ", pluginArgs);
+
+            if (MethodTimer.PatchPlugin(pluginName))
+            {
+                response = $"Started profiling '{pluginName}'.";
                 return true;
             }
             else
             {
-                MethodTimer.PatchAssembly(string.Join(" ", args[0]));
-                response = $"Patched";
+                response = $"Failed to start profiling '{pluginName}' (already patched or not found).";
+                return false;
+            }
+        }
+
+        private bool HandleUnpatch(List<string> pluginArgs, out string response)
+        {
+            if (pluginArgs.Count == 0)
+            {
+                response = "Error: No plugin name specified. Usage: debugtiming unpatch <plugin name>";
+                return false;
+            }
+
+            string pluginName = string.Join(" ", pluginArgs);
+
+            if (MethodTimer.UnpatchPlugin(pluginName))
+            {
+                response = $"Stopped profiling '{pluginName}'.";
                 return true;
             }
+            else
+            {
+                response = $"Failed to stop profiling '{pluginName}' (not currently patched).";
+                return false;
+            }
+        }
+
+        private bool HandleList(out string response)
+        {
+            if (!MethodTimer.IsPatched)
+            {
+                response = "No plugins are currently being profiled.";
+                return true;
+            }
+
+            var patchedPlugins = MethodTimer.PatchedPluginNames.ToList();
+            response = $"Currently profiling {patchedPlugins.Count} plugin(s):\n" +
+                      string.Join("\n", patchedPlugins.Select(p => $"• {p}"));
+            return true;
+        }
+
+        private bool HandleUnpatchAll(out string response)
+        {
+            if (!MethodTimer.IsPatched)
+            {
+                response = "No plugins are currently being profiled.";
+                return true;
+            }
+
+            var count = MethodTimer.PatchedPluginNames.Count;
+            MethodTimer.UnpatchAll();
+            response = $"Stopped profiling all {count} plugin(s).";
+            return true;
+        }
+
+        private bool HandleHelp(out string response)
+        {
+            response = "DebugTiming Command Usage:\n\n" +
+                      "Actions:\n" +
+                      "  patch <plugin name>              - Start profiling specified plugin\n" +
+                      "  unpatch <plugin name>            - Stop profiling specified plugin\n" +
+                      "  list / status                    - Show currently profiled plugins\n" +
+                      "  unpackall / clear / stop         - Stop profiling all plugins\n" +
+                      "  help / ?                         - Show this help message\n\n" +
+                      "Aliases for actions:\n" +
+                      "  patch: enable, on\n" +
+                      "  unpatch: disable, off\n\n" +
+                      "Examples:\n" +
+                      "  debugtiming patch MyPlugin\n" +
+                      "  debugtiming unpatch MyPlugin\n" +
+                      "  debugtiming list\n" +
+                      "  debugtiming unpackall\n\n";
+            return true;
         }
     }
 }
--- DebugTools/Harmony/Patches/MethodTimer.cs
+++ DebugTools/Harmony/Patches/MethodTimer.cs
@@ -1,11 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading;
 using HarmonyLib;
-using LabApi.Features.Console;
 using LabApi.Features.Wrappers;
 using LabApi.Loader;
 
@@ -14,18 +14,41 @@ namespace DebugTools.Commands.Patches
     public static class MethodTimer
     {
         private static readonly StringBuilder sb = new();
-        private static Harmony harmony;
-        internal static bool isPatched = false;
 
-        public static void PatchAssembly(string pluginName)
+        private static readonly Dictionary<string, Harmony> harmonyInstances = [];
+        private static readonly Dictionary<string, LabApi.Loader.Features.Plugins.Plugin> patchedPlugins = new();
+
+        public static bool IsPatched => patchedPlugins.Count > 0;
+
+        public static IReadOnlyCollection<string> PatchedPluginNames => patchedPlugins.Keys;
+
+        public static bool PatchPlugin(string pluginName)
         {
-            if (isPatched)
-                return;
+            if (patchedPlugins.ContainsKey(pluginName))
+            {
+                Logger.Debug($"Plugin '{pluginName}' is already patched.");
+                return false;
+            }
 
-            PluginLoader.Plugins.TryGetValue(PluginLoader.Plugins.Keys.Where(p => p.Name == pluginName).FirstOrDefault(), out Assembly value);
-            harmony = new Harmony($"com.ucs.uci_labapi-debugging-{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}");
+            var pluginKey = PluginLoader.Plugins.Keys.FirstOrDefault(p => p.Name == pluginName);
+            if (pluginKey == null)
+            {
+                Logger.Debug($"Plugin '{pluginName}' not found.");
+                return false;
+            }
 
-            foreach (Type type in value.GetTypes())
+            if (!PluginLoader.Plugins.TryGetValue(pluginKey, out Assembly assembly))
+            {
+                Logger.Debug($"Failed to get assembly for plugin '{pluginName}'.");
+                return false;
+            }
+
+            string harmonyId = $"debugtools-methodtimer-{pluginName}-{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
+            Harmony harmony = new(harmonyId);
+
+            int patchedMethodCount = 0;
+
+            foreach (Type type in assembly.GetTypes())
             {
                 if (type == typeof(MethodTimer))
                     continue;
@@ -40,43 +63,68 @@ namespace DebugTools.Commands.Patches
                         HarmonyMethod prefix = new(typeof(MethodTimer).GetMethod(nameof(Prefix)));
                         HarmonyMethod postfix = new(typeof(MethodTimer).GetMethod(nameof(Postfix)));
                         harmony.Patch(method, prefix, postfix);
+                        patchedMethodCount++;
                     }
-                    catch
+                    catch (Exception ex)
                     {
-
+                        Logger.Debug($"Failed to patch {type.Name}.{method.Name}: {ex.Message}");
                     }
                 }
             }
 
-            isPatched = true;
-            Logger.Debug("MethodTimer profiling has been enabled.");
+            harmonyInstances[pluginName] = harmony;
+            patchedPlugins[pluginName] = pluginKey;
+
+            Logger.Debug($"MethodTimer profiling enabled for plugin '{pluginName}' ({patchedMethodCount} methods patched).");
+            return true;
+        }
+
+        public static bool UnpatchPlugin(string pluginName)
+        {
+            if (!patchedPlugins.ContainsKey(pluginName))
+            {
+                Logger.Debug($"Plugin '{pluginName}' is not currently patched.");
+                return false;
+            }
+
+            if (harmonyInstances.TryGetValue(pluginName, out Harmony harmony))
+            {
+                harmony.UnpatchAll(harmony.Id);
+                harmonyInstances.Remove(pluginName);
+                patchedPlugins.Remove(pluginName);
+
+                Logger.Debug($"MethodTimer profiling disabled for plugin '{pluginName}'.");
+                return true;
+            }
+
+            return false;
         }
 
         public static void UnpatchAll()
         {
-            if (!isPatched || harmony == null)
-                return;
+            var pluginNames = patchedPlugins.Keys.ToList();
 
-            harmony.UnpatchAll(harmony.Id);
-            isPatched = false;
-            Logger.Debug("MethodTimer profiling has been disabled.");
+            foreach (string pluginName in pluginNames)
+                UnpatchPlugin(pluginName);
+
+            Logger.Debug("MethodTimer profiling has been disabled for all plugins.");
         }
 
         public static void Prefix(out Stopwatch __state)
         {
-            sb.Clear();
             __state = Stopwatch.StartNew();
         }
 
         public static void Postfix(Stopwatch __state, MethodBase __originalMethod)
         {
             __state.Stop();
-            if (__state.Elapsed.TotalMilliseconds > .3)
+            if (__state.Elapsed.TotalMilliseconds > 0.3)
             {
-                sb.AppendLine();
-                sb.AppendLine("Thread | RoundTime | TPS | Max TPS | MethodInfo");
-                sb.AppendLine($"{Thread.CurrentThread.ManagedThreadId} - {Round.Duration.TotalSeconds} - {Server.Tps} - {Server.MaxTps} - {__originalMethod.DeclaringType}.{__originalMethod.Name} took {__state.Elapsed.TotalMilliseconds:F2} ms");
-                Logger.Debug(sb);
+                var localSb = new StringBuilder();
+                localSb.AppendLine();
+                localSb.AppendLine("Thread | RoundTime | TPS | Max TPS | MethodInfo");
+                localSb.AppendLine($"{Thread.CurrentThread.ManagedThreadId} - {Round.Duration.TotalSeconds} - {Server.Tps} - {Server.MaxTps} - {__originalMethod.DeclaringType}.{__originalMethod.Name} took {__state.Elapsed.TotalMilliseconds:F2} ms");
+                Logger.Debug(localSb);
             }
         }
     }
